d55e22467b01573cfb8247a988e5c9e70f6f13d5
Jakub Sielewicz <76447210+sielewa@users.noreply.github.com>
2022-03-09 17:59:24 +0100

    some changes

--- store/users.js
+++ store/users.js
@@ -1,5 +1,5 @@
 export const actions = {
     async createUser ({ commit }, payload) {
-        const res = await this.$axios.$post('/api/users/hello', payload)
+        const res = await this.$axios.$post('/api/users', payload)
     }
 }
