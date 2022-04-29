76a2cdd19366484f5f9e57517ffd72d98da35b8b
Jakub Sielewicz <sielewa@jakubs-macbook-pro.home>
2022-04-29 18:42:19 +0200

    Some corrections

--- components/QuotesContainer.vue
+++ components/QuotesContainer.vue
@@ -44,21 +44,6 @@ export default {
 			})
 
 			return this.quotesData;
-			/*
-      this.quotesData = this.quotes;
-      for (const quote of this.quotesData) {
-        let dateComponents = quote.created_at.split("T");
-        let date = dateComponents[0];
-        let time = dateComponents[1].split(".")[0];
-        quote.date = date;
-        quote.time = time;
-        quote.isAuthor = false;
-
-        if (quote.username === this.$store.getters["users/getUser"]) {
-          quote.isAuthor = true;
-        }
-      }
-      return this.quotesData;*/
     },
   },
 };
